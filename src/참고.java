e4ee629fb23c95153bf9155318e7e246f5b0b39a
unul0912 <89674578+unul0912@users.noreply.github.com>
2022-01-27 04:21:44 +0900

    Initial commit

---
+++ src/참고.java
@@ -0,0 +1,25 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class 참고 {
+
+    public static void main (String[] args) throws IOException {
+        // throws IOException
+        //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        /*int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        Integer.parseInt(st.nextToken());
+        StringBuilder sb = new StringBuilder();
+        sb.append(i).append("\n");*/
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+
+
+    }
+}
