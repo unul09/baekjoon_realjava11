6930a65476fd9612b9142c0e1d65f96d5209005b
unul0912 <89674578+unul0912@users.noreply.github.com>
2022-01-28 10:42:42 +0900

    11653 (나중에 스스로 다시해볼것)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -17,31 +17,34 @@ public class Main {
     소수 2부터 시작해서,
     N%소수=0이라면 N 소수로 나눠주고 반복,
     아니라면 다음 소수값(ex) 2 -> 3)으로 반복
+
+    시간초과 해결방안?
+    sb -> X
+    루트 N 할때마다 실행? -> O !!
+
+    Stranger's lab 참고해서 작성한 내용임. 나중에 스스로 다시 해결해볼 것.
+
      */
 
 
     public static void primeFactorization(int N){
-        /*소수 2부터 시작해서,
-        N%소수=0이라면 N 소수로 나눠주고 해당소수 출력 후 반복,
-        아니라면 다음 소수값(ex) 2 -> 3)으로 반복*/
-        int prime = 2;
-        while(N != 1){
-            if(N % prime == 0){
-                N = N/prime;
-                System.out.println(prime);
-            }
-            else{
-                prime = nextPrimeNum(prime);
-            }
-        }
-    }
 
-    public static int nextPrimeNum(int currentPrime){ //다음 소수값 찾아서 반환
-        currentPrime++;
-        while(isPrimeNum(currentPrime) == false){
-            currentPrime++;
+        /*
+        기본 틀: N = a*b 일때, 반드시 한 수는 루트N보다 작거나 같다
+        루트N에 대해 실행. 새롭게 나누어진 N값에 대해 계속 적용된다
+        9991의 경우, 97 구하고 103은 계산할 필요 없이 출력되기 때문에 시간 단축 굳.
+         */
+        for(int i=2; i<= Math.sqrt(N); i++){
+            while(N % i == 0){
+                System.out.println(i);
+                N /= i;
+            }
         }
-        return currentPrime;
+
+        if(N!= 1){
+            System.out.println(N);
+        }
+
     }
 
 
